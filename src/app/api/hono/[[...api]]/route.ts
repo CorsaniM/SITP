4bc5d11f3d7a1729550c3d4ce0df0bafd9d9a2f8
Maximo <MaximoCorsani@gmail.com>
2024-08-08 16:03:43 -0300

    push

--- src/app/api/hono/[[...api]]/route.ts
+++ src/app/api/hono/[[...api]]/route.ts
@@ -1,30 +1,30 @@
-import { Hono } from 'hono';
-import { api } from '~/trpc/server';
+import { Hono } from "hono";
+import { api } from "~/trpc/server";
 
 const app = new Hono().basePath("api/hono");
 
-app.get('/ticket/:id', async (c, next) => {
-    const ticketId = c.req.param('id')
+app.get("/ticket/:id", async (c) => {
+  const ticketId = parseInt(c.req.param("id"));
 
-    const ticket = api.tickets.getById({ id: parseInt(ticketId) });
-    if (ticket){
-        return c.json(ticket)
+  try {
+    const ticket = await api.tickets.getById({ id: ticketId });
+    if (ticket) {
+      return c.json(ticket);
     } else {
-        return c.json('no existe el ticket ' + ticketId)
+      return c.json({ message: "No existe el ticket " + ticketId });
     }
-})
-
-
+  } catch (error) {
+    console.error("Error fetching ticket:", error);
+    return c.json({ message: "Error al obtener el ticket", error }, 500);
+  }
+});
 
 app.notFound((c) => {
-    return c.text('Custom 404 Message', 404)
-  })
+  return c.text("Custom 404 Message", 404);
+});
 
-  export const GET = app.fetch;
-  export const POST = app.fetch;
-  export const PUT = app.fetch;
-  export const DELETE = app.fetch;
-  export const PATCH = app.fetch;
-
-//app.get('/posts/:id/comment/:comment_id', (c) => {
-//    const { id, comment_id } = c.req.param()  ... })
+export const GET = app.fetch;
+export const POST = app.fetch;
+export const PUT = app.fetch;
+export const DELETE = app.fetch;
+export const PATCH = app.fetch;
