a333f1bdb5e81aca3704a33b2ace679cb614c4a3
CorsaniM <maximocorsani@gmail.com>
2024-10-04 11:03:56 -0300

    ,

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/(.*)", "/forum(.*)"]);
 const isWebhookRoute = createRouteMatcher(["/api/uploadthing(.*)"]);
+const isWebhookRouteApi = createRouteMatcher(["/api/hono(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   // Permitir acceso a la ruta del webhook
